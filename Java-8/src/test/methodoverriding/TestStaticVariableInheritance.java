f05b16a35234f1dc4f9c2a7e263ccfcc27440168
admin <admin@admin-PC>
2019-03-17 01:01:00 -0700

    Overriding changes committed.

--- Java-8/src/test/methodoverriding/TestStaticVariableInheritance.java
+++ Java-8/src/test/methodoverriding/TestStaticVariableInheritance.java
@@ -9,6 +9,9 @@ public class TestStaticVariableInheritance extends Parent {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(x);
+		x = 11;
+		System.out.println(x);
+		System.out.println(TestStaticVariableInheritance.x);
 	}
 
 }
